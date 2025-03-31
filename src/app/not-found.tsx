33f350a69eb54253766b1cfd1ce0c81ec00e0776
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-04-01 05:19:24 +0530

    resolving deployement errors

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,17 @@
+
+import Link from "next/link"
+
 export default function NotFound() {
     return (
       <div className="flex min-h-screen flex-col items-center justify-center">
         <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
         <p className="text-lg text-muted-foreground mb-8">The page you are looking for does not exist.</p>
-        <a
-          href="/"
-          className="px-4 py-2 bg-primary text-primary-foreground rounded-md hover:bg-primary/90 transition-colors"
-        >
-          Go back home
-        </a>
+        <Link
+        href="/"
+        className="px-4 py-2 bg-primary text-primary-foreground rounded-md hover:bg-primary/90 transition-colors"
+      >
+        Go back home
+      </Link>
       </div>
     )
   }
